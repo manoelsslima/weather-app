051ad627f3582d232a6f20942e1c02697a1fceba
Manoel Lima <manoelsslima@yahoo.com.br>
2024-01-03 02:05:25 -0400

    consuming api

--- src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Weather } from 'src/app/models/interfaces/Weather';
+import { WeatherService } from './../../services/weather.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-weather-home',
-  templateUrl: './weather-home.component.html',
-  styleUrls: ['./weather-home.component.scss']
+  templateUrl: './weather-home.component.html'
 })
-export class WeatherHomeComponent {
+export class WeatherHomeComponent implements OnInit {
 
+  initialCityName: string = 'Manaus';
+  weatherData!: Weather;
+
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit(): void {
+    this.getWeather(this.initialCityName);
+  }
+
+  getWeather(cityName: string):void {
+    this.weatherService.getWeatherData(cityName)
+    .subscribe({
+      next: (response) => {
+        // se po response existe
+        response && (this.weatherData = response);
+        console.log(this.weatherData.main.temp);
+      },
+      error: (error) => console.log(error),
+    })
+  }
 }
